b366ba12c2022dc62d5519b75f44419f86eef814
Zsofia Matyasi <zsofia.matyasi@gmail.com>
2021-11-15 11:39:23 +0100

    [feat] create basic skeleton for project

---
+++ src/containers/Pokemon.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// export default function Pokemon() {
+//     return (
+//         <div>
+
+//         </div>
+//     )
+// }
+
+const Pokemon = () => {
+  return <div>Pokemon</div>;
+};
+
+export default Pokemon;
---
+++ src/containers/PokemonList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// export default function pokemonList() {
+//     return (
+//         <div>
+
+//         </div>
+//     )
+// }
+const PokemonList = () => {
+  return <div>PokemonList</div>;
+};
+
+export default PokemonList;
